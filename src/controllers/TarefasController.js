4b86b40bc3cc169af67ed8d9d22de84f0747dc53
Roberto sousa <robertosousa186@gmail.com>
2020-05-18 02:22:40 -0300

    Atualizando CRUD básico das tarefas.

--- src/controllers/TarefasController.js
+++ src/controllers/TarefasController.js
@@ -1,12 +1,6 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(request, response) {
-        const tarefas = await connection('tarefas').select('*');
-
-        return response.json(tarefas);
-    },
-
     async create(request, response, next) {
         try {
             const {
@@ -18,10 +12,55 @@ module.exports = {
             await connection('tarefas').insert({
                 nome,
                 descricao,
-                urgencia,   
+                urgencia,
             });
 
             return response.status(201).send();
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    async index(request, response) {
+        const tarefas = await connection('tarefas').select('*');
+        return response.json(tarefas);
+    },
+
+    async update(request, response, next) {
+        try {
+            const {
+                nome,
+                descricao,
+                urgencia
+            } = request.body;
+
+            const {id} = request.params;
+
+            await connection('tarefas')
+                .update({
+                    nome,
+                    descricao,
+                    urgencia
+                })
+                .where({id});
+
+            return response.send();
+
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    async delete(request, response, next){
+        try {
+            const {id} = request.params;
+
+            await connection('tarefas')
+            .where({id})
+            .delete();
+            
+            return response.send();
+            
         } catch (error) {
             next(error);
         }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ const TaferasController = require('./controllers/TarefasController');
 
 const routes = express.Router();
 
-routes.get('/listar-tarefas', TaferasController.index);
 routes.post('/tarefas', TaferasController.create);
+routes.get('/tarefas', TaferasController.index);
+routes.put('/tarefas/:id', TaferasController.update);
+routes.delete('/tarefas/:id', TaferasController.delete);
 
 module.exports = routes;
